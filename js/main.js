d2fdcec4a833fbd10ab3fb9325d45e00d6cb08a8
svidovskaya <43477425+svidovskaya@users.noreply.github.com>
2019-03-19 13:38:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function info(){
-console.log(document.getElementById("txt").value)
+console.log("Значение 1: " + document.getElementById("txt").value + '\n' + "Значение 2: " + document.getElementById("txt2").value + '\n' + "Значение 3: " + document.getElementById("txt3").value)
 }
 document.getElementById("btn").onclick = info;
